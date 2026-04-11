bd581072f41529746459d11e39d4ca3ffe046371
Peter Siegel <psiegel2000@icloud.com>
2026-04-11 15:04:53 +0200

    feat: add setup.py for building cuda extensions in vision3d

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""setuptools entry point for vision3d's C++ extension."""
+
+from pathlib import Path
+
+from setuptools import setup
+from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CUDAExtension
+
+_HAS_CUDA = CUDA_HOME is not None
+
+_ROOT = Path(__file__).resolve().parent
+_CSRC = _ROOT / "src/vision3d/ops/csrc"
+_SOURCES = [
+    "src/vision3d/ops/csrc/register.cpp",
+    "src/vision3d/ops/csrc/iou_box3d/iou_box3d_cpu.cpp",
+]
+if _HAS_CUDA:
+    _SOURCES.append("src/vision3d/ops/csrc/iou_box3d/iou_box3d.cu")
+
+setup(
+    ext_modules=[
+        CUDAExtension(
+            name="vision3d._C",
+            sources=_SOURCES,
+            include_dirs=[str(_CSRC)],
+            define_macros=[("WITH_CUDA", None)] if _HAS_CUDA else [],
+        ),
+    ],
+    cmdclass={"build_ext": BuildExtension},
+)
