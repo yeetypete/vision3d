ea0ed2bc16e9037f8a6bc673b76563fa9d95c1a0
Peter Siegel <psiegel2000@icloud.com>
2026-04-11 23:43:25 +0200

    feat: add 3d_nms implementations

--- src/vision3d/ops/__init__.py
+++ src/vision3d/ops/__init__.py
@@ -1,15 +1,18 @@
 from ._box3d_corners import box3d_corners
 from ._box3d_iou import box3d_iou
 from ._box3d_overlap import box3d_overlap
+from ._nms_3d import batched_nms_3d, nms_3d
 from ._points_in_boxes_3d import points_in_boxes_3d, points_in_boxes_3d_indices
 from ._project import project_to_image
 from .boxes3d import box3d_convert
 
 __all__ = [
+    "batched_nms_3d",
     "box3d_convert",
     "box3d_corners",
     "box3d_iou",
     "box3d_overlap",
+    "nms_3d",
     "points_in_boxes_3d",
     "points_in_boxes_3d_indices",
     "project_to_image",
