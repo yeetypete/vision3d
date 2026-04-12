280423a0ffc02f34f6ee8b3fa538d010b35137c8
Peter Siegel <psiegel2000@icloud.com>
2026-04-12 14:53:31 +0200

    feat: add point cloud transforms

--- src/vision3d/transforms/__init__.py
+++ src/vision3d/transforms/__init__.py
@@ -1,9 +1,13 @@
 from ._copy_paste_3d import CopyPaste3D
 from ._geometry import RandomFlip3D, RandomRotate3D, RandomScale3D, RandomTranslate3D
+from ._point_cloud import PointJitter, PointSample, PointShuffle
 from ._transform import RandomTransform, Transform
 
 __all__ = [
     "CopyPaste3D",
+    "PointJitter",
+    "PointSample",
+    "PointShuffle",
     "RandomFlip3D",
     "RandomRotate3D",
     "RandomScale3D",
