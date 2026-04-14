d0471cfb70281182d37f95dc1c512844ac06a7ee
Peter Siegel <psiegel2000@icloud.com>
2026-04-14 21:53:09 +0200

    feat: add better options for autodoc and napoleon api rendering

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,28 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "show-inheritance": True,
+}
+autodoc_inherit_docstrings = False
+autodoc_member_order = "bysource"
+autodoc_preserve_defaults = True
+autodoc_typehints = "description"
+
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
+napoleon_preprocess_types = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_ivar = True
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "torch": ("https://docs.pytorch.org/docs/stable/", None),
+    "torchvision": ("https://docs.pytorch.org/vision/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
 
 templates_path = ["_templates"]
 
