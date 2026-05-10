665737bf585c05dfa09773d89580812fd1dcdefe
Peter Siegel <psiegel2000@icloud.com>
2026-05-10 09:55:50 +0200

    fix: add rerun docs to intersphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,7 @@ intersphinx_mapping = {
     "torch": ("https://docs.pytorch.org/docs/stable/", None),
     "torchvision": ("https://docs.pytorch.org/vision/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
+    "rerun": ("https://ref.rerun.io/docs/python/stable/", None),
 }
 
 templates_path = ["_templates"]
